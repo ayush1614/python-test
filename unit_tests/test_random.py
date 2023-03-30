38f8fd120ce2d511bbe9588dd7ec2d26aec635c3
Ayush Katiyar <katiyarayush2000@gmail.com>
2023-03-31 00:23:24 +0530

    added new test file to check pr decoration

---
+++ unit_tests/test_random.py
@@ -0,0 +1,25 @@
+import unittest
+
+class TestRandoms(unittest.TestCase):
+    def setUp(self):
+        pass
+    
+    def test_stringfunctions(self):
+        self.assertEqual('A','a'.upper())
+        self.assertEqual('B','b'.upper())
+        self.assertEqual('C','c'.upper())
+        self.assertEqual('D','d'.upper())
+        self.assertEqual('E','e'.upper())
+        self.assertEqual('F','f'.upper())
+        self.assertEqual('G','g'.upper())
+        self.assertEqual('H','h'.upper())
+        self.assertEqual('I','i'.upper())
+        self.assertEqual('J','j'.upper())
+        self.assertEqual('K','k'.upper())
+        self.assertEqual('L','l'.upper())
+        self.assertEqual('M','m'.upper())
+        self.assertEqual('N','n'.upper())
+        self.assertEqual('I','i'.upper())
+        self.assertEqual('I','i'.upper())
+        self.assertEqual('P','p'.upper())
+        self.assertEqual('Q','q'.upper())
